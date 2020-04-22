017a6a0968488cf87f2ea979eb22e91dc84f1a8c
Shilpa Soni <shilpassoni@gmail.com>
2020-04-22 16:35:31 +0530

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
+    title: `Gatsby Personal Blog`,
     author: `Kyle Matthews`,
     description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
